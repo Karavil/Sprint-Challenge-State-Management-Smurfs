be9d7f525ded087b0bb869e131a01bac7ca71f77
Alex Karavil <alex@karavil.io>
2020-03-20 11:14:26 -0400

    Initial setup for redux is complete

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+   smurfs: []
+};
+
+const smurfReducer = (state = initialState, { type, payload }) => {
+   switch (type) {
+      default:
+         state = { ...state };
+   }
+};
+
+export default smurfReducer;
